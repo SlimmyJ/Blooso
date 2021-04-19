7b34148299cbf2fedb582d457757d48294362b3a
Simon Van Tittelboom <simon.van.tittelboom@gmail.com>
2021-04-19 10:31:40 +0200

    Add project files.

---
+++ Blooso/Blooso/App.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+using Blooso.Services;
+using Blooso.Views;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Blooso
+    {
+    public partial class App : Application
+        {
+
+        public App()
+            {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new AppShell();
+            }
+
+        protected override void OnStart()
+            {
+            }
+
+        protected override void OnSleep()
+            {
+            }
+
+        protected override void OnResume()
+            {
+            }
+        }
+    }
---
+++ Blooso/Blooso/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+
+using Blooso.ViewModels;
+
+using Xamarin.Forms;
+
+namespace Blooso.Views
+    {
+    public partial class ItemDetailPage : ContentPage
+        {
+        public ItemDetailPage()
+            {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+            }
+        }
+    }
