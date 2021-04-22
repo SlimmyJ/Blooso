c34f5137b7f9d989bb55a18fce4ba09ddb23ed65
Simon Van Tittelboom <simon.van.tittelboom@gmail.com>
2021-04-22 10:58:26 +0200

    Danger UI - working

---
+++ Blooso/Blooso/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Blooso.Interfaces;
+using Blooso.Models;
+
+namespace Blooso.ViewModels
+{
+    public class RegisterViewModel : BaseViewModel
+    {
+        public IUserRepository UserRepository;
+        public User UserToBeRegistered;
+    }
+}
